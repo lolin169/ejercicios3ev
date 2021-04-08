f8bdbe893cb697bc228cfda8455c8971c3ffce89
Manuel Ramos <lolin169@gmail.com>
2021-04-08 17:08:12 +0200

    Ejercicio 3 de la unidad 8 (colecciones) resuelto

---
+++ src/unidad8/colecciones/Ejercicio3.java
@@ -0,0 +1,23 @@
+package unidad8.colecciones;
+
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		String linea = teclado.nextLine();
+		Set<String> norepeat = new TreeSet<>();
+		Set<String> repeat = new TreeSet<>();
+		for (String palabra : linea.split("\\P{L}+")) {
+			if (!norepeat.add(palabra))
+				repeat.add(palabra);
+		}
+		norepeat.removeAll(repeat);
+		System.out.println("Palabras  NO repetidas: " + norepeat);
+		System.out.println("Palabras  repetidas: " + repeat);
+	}
+
+}
