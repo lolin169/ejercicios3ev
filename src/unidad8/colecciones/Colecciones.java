dc155e493b6064c57992b0f3bd6147ce4cc2c055
Manuel Ramos <lolin169@gmail.com>
2021-04-15 16:55:57 +0200

    Ejercicio 2 de la unidad 8 (colecciones 2) resuelto

--- src/unidad8/colecciones/Colecciones.java
+++ src/unidad8/colecciones/Colecciones.java
@@ -2,6 +2,7 @@ package unidad8.colecciones;
 
 import java.util.Iterator;
 import java.util.Set;
+import java.util.function.Predicate;
 
 public class Colecciones {
 
@@ -13,6 +14,40 @@ public class Colecciones {
 //		while(i.hasNext())
 //			if(i.next().length()%2==0)
 //				i.remove();
+	}
+
+	static boolean contieneImpares(Set<Integer> numeros) {
+//		Una manera de hacerlo:
+//		
+//		Predicate<Integer> n = new Predicate<Integer>() {
+//			@Override
+//			public boolean test(Integer t) {
+//				if (t % 2 == 1)
+//					return true;
+//				return false;
+//			};
+//		};
+//		for (int x : numeros)
+//			if (n.test(x))
+//				return true;
+//		return false;
+
+//		Otra manera:
+
+//		Iterator<Integer> i= numeros.iterator();
+//		while(i.hasNext()) {
+//			if(i.next()%2==1)
+//				return true;
+//		}
+//		return false;
+
+//		Otra:
+
+		for (int n : numeros) {
+			if (n % 2 != 0)
+				return true;
+		}
+		return false;
 	}
 
 }
