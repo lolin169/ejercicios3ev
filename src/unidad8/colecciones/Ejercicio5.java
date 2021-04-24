696e1fbd461ae4d88d58645ab5bb22f88509dbb9
Manuel Ramos <lolin169@gmail.com>
2021-04-24 18:35:55 +0200

    Ejercicio 5 de la unidad 8 (colecciones) corregido

--- src/unidad8/colecciones/Ejercicio5.java
+++ src/unidad8/colecciones/Ejercicio5.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.NoSuchElementException;
 import java.util.Queue;
 import java.util.Scanner;
 
@@ -14,14 +15,16 @@ public class Ejercicio5 {
 	static Scanner linea = new Scanner(in);
 
 	public static void main(String[] args) throws IOException {
+		try {
 		int N = linea.nextInt();
 		int K = linea.nextInt();
 		int X = linea.nextInt();
 		linea = new Scanner(in.readLine());
 
 		for (int i = 0; i < N; i++) {
-			cola.add(linea.nextInt());
+			cola.offer(linea.nextInt());
 		}
+		
 
 		Iterator<Integer> i = cola.iterator();
 		while (K != 0 && !cola.isEmpty()) {
@@ -32,7 +35,7 @@ public class Ejercicio5 {
 
 		if (cola.contains(X))
 			System.out.println("true");
-		else {
+		else if(!cola.isEmpty()) {
 			int min;
 			min = cola.peek();
 			for (int a : cola) {
@@ -40,6 +43,10 @@ public class Ejercicio5 {
 					min = a;
 			}
 			System.out.println(min);
+		}else
+			System.out.println("0");
+		}catch(NoSuchElementException e) {
+			System.out.println("No hay valores que meter en la cola");
 		}
 	}
 }
