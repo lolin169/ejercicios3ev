d97c11c19e53e0a74bfb8e2b131d4543d4a009da
Manuel Ramos <lolin169@gmail.com>
2021-05-20 16:32:31 +0200

    Ejercicio 1 de la unidad 8 (ficheros) resuelto

---
+++ src/unidad8/ficheros/Ejercicio1.java
@@ -0,0 +1,29 @@
+package unidad8.ficheros;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		File nombre;
+		if (args.length == 1)
+			nombre = new File(args[0]);
+		else {
+			Scanner teclado = new Scanner(System.in);
+			nombre = new File("C:\\Users\\Usuario\\Desktop\\Archivos\\El Quijote ISO-8859-1.txt");
+			teclado.close();
+		}
+		if (!nombre.exists())
+			System.out.println("La ruta introducida no existe o es errónea");
+		else {
+			if (nombre.isDirectory())
+				System.out.println("El nombre del DIRECTORIO es: " + nombre.getName());
+			else
+				System.out.println("El nombre del ARCHIVO es: " + nombre.getName() + "\nEl tamaño es de: "
+						+ nombre.length() + " bytes");
+			System.out.println("Los permisos son: " + (nombre.canExecute() ? "EJECUTABLE " : "")
+					+ (nombre.canRead() ? "LECTURA " : "") + (nombre.canWrite() ? " ESCRITURA." : "."));
+		}
+	}
+}
