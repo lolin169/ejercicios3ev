def1221736b8d56f0ab953d69fad72162e991439
Manuel Ramos <lolin169@gmail.com>
2021-05-20 17:57:24 +0200

    Ejercicio 4 de la unidad 8 (ficheros) resuelto

---
+++ src/unidad8/ficheros/Ejercicio4.java
@@ -0,0 +1,62 @@
+package unidad8.ficheros;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		String path;
+		int lineas;
+		int palabras;
+		int letras;
+		if (args.length < 1)
+			path = teclado.nextLine();
+		else
+			path = args[0];
+		teclado.close();
+		try (BufferedReader lector = new BufferedReader(new FileReader(path))) {
+			String linea;
+			lineas = 0;
+			palabras = 0;
+			letras = 0;
+			while ((linea = lector.readLine()) != null) {
+				lineas++;
+				for (String palabra : linea.split("\\P{L}+")) {
+					palabras++;
+					letras += palabra.length();
+				}
+			}
+			System.out.println(
+					"El archivo tiene " + lineas + " lineas, " + palabras + " palabras, " + letras + " letras.");
+			DataOutputStream out = new DataOutputStream(
+					new BufferedOutputStream(new FileOutputStream("ejercicio4.bin")));
+			out.writeUTF(path);
+			out.writeInt(lineas);
+			out.writeInt(palabras);
+			out.writeInt(letras);
+			out.close();
+			System.out.println("Archivo binario creado y escrito correctamente");
+			DataInputStream in = new DataInputStream(new BufferedInputStream(new FileInputStream("ejercicio4.bin")));
+			System.out.println(in.readUTF() + " " + in.readInt() + " lineas, " + in.readInt() + " palabras, "
+					+ in.readInt() + " letras.");
+			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+//    archivos\El Quijote UTF-8.txt
